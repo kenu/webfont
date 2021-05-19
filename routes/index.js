2e6d235cf3373fdda785f9fb71fa5c68409526c2
kenu <kenu@okjsp.pe.kr>
2021-05-19 14:40:37 +0900

    add 노회찬, DancingScript

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '한글 폰트 Express' });
+  res.render('index', { title: '손글씨 폰트 Express' });
 });
 
 module.exports = router;
